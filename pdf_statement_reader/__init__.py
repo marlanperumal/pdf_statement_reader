9a0be3fc2d4a3d6862a1f8e6f6ccfbe086728fe9
Marlan Perumal <marlan.perumal@gmail.com>
2019-02-10 11:16:13 +0200

    fixed options on decrypt_pdf cli

--- pdf_statement_reader/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from tabula import read_pdf
-import pandas as pd
-import numpy as np
-
-
-
--- pdf_statement_reader/decrypt.py
+++ pdf_statement_reader/decrypt.py
@@ -2,28 +2,31 @@ from pikepdf import Pdf
 import click
 
 
-@click.commnand()
-@click.option("--input", "-i", "input_filename")
-@click.option("--output", "-o", "output_filename")
-@click.option("--password", "-p", prompt=True, hide_input=True)
+@click.command()
+@click.argument("input_filename", type=click.Path(exists=True))
+@click.argument("output_filename", type=click.Path(), required=False)
+@click.option('--password', "-p", prompt=True, hide_input=True)
 def decrypt_pdf(input_filename, output_filename=None, password=None):
     """Decrypts a pdf file
 
     Uses pikepdf to open an encrypted pdf file and then save the unencrypted version.
-    If not output_filename is specified then overwrites the original file.
+    If no output_filename is specified then overwrites the original file.
     If no password is supplied then checks if file is already unencrypted
 
+    \b
     Args:
         input_filename: The source encrypted pdf file
         output_filename: The filename to write the decrypted file to
         password: The password on the encrypted pdf file
 
+    \b
     Returns:
         True if decryption succeeded, False if the file was already unencrypted.
         If decryption fails, an error is raised
 
+    \b
     Raises:
-        FileNotFoundError: if the input file does not exist or output cannot be written
+        FileNotFoundError: if output cannot be written
         PdfError: if the file is not a valid pdf
         PasswordError: if incorrect password supplied
     """
